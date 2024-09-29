722cea06f187c55a12b317db0e7e2211ac21c4ae
Rémi Verchère <remi@verchere.fr>
2024-09-29 22:40:57 +0200

    add demo-magic scripts
    Signed-off-by: Rémi Verchère <remi@verchere.fr>

---
+++ 10-scripts/01-sdkman.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+########################
+# include the magic
+# https://github.com/paxtonhare/demo-magic
+########################
+. demo-magic.sh
+clear
+
+export KUBECONFIG=${PWD}/mks-1-kubeconfig
+
+# Set demo-magic options
+#TYPE_SPEED=50 # Accelerate typing
+DEMO_CMD_COLOR="" # No bold
+DEMO_PROMPT="${PURPLE}$ ${COLOR_RESET}"
+DEMO_COMMENT_COLOR=$CYAN
+
+# Save original PWD
+_PWD=${PWD}
+
+# Download sdkman
+pi "# Download and install sdkman"
+pe "curl -s https://get.sdkman.io | bash"
+pei "source $HOME/.sdkman/bin/sdkman-init.sh"
+pei "sdk version"
+
+# Install sdk versions
+pi "# Install some java"
+pe "sdk install java 17-open"
+pei "sdk install java 21.0.2-open"
+pei "sdk install quarkus"
+pei "sdk install java 21.0.2-graalce"
+
+pi "# Use java 17"
+pe "sdk use java 17-open"
+pei "java -version"
+
+# to set the default version of java
+pi "# Use java 21, as default"
+pe "sdk default java 21.0.2-open"
+pei "sdk use java 21.0.2-open"
+pei "java -version"
+
+pi "# End"
+# Return to the default PWD
+cd ${_PWD}
+
+pe ""
---
+++ 10-scripts/02-quarkus.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+########################
+# include the magic
+# https://github.com/paxtonhare/demo-magic
+########################
+. demo-magic.sh
+clear
+
+export KUBECONFIG=${PWD}/mks-1-kubeconfig
+
+# Set demo-magic options
+#TYPE_SPEED=50 # Accelerate typing
+DEMO_CMD_COLOR="" # No bold
+DEMO_PROMPT="${PURPLE}$ ${COLOR_RESET}"
+DEMO_COMMENT_COLOR=$CYAN
+
+# Save original PWD
+_PWD=${PWD}
+
+cd ../02-quarkus/
+source $HOME/.sdkman/bin/sdkman-init.sh
+
+pi "# Activate Graalvm"
+pe "sdk use java 21.0.2-graalce"
+pei "java -version"
+
+# Build image
+pi "# Build Native image"
+pe "./mvnw clean package -Dnative -DskipTests"
+pei " ./target/qute-quickstart-1.0.1-SNAPSHOT-runner"
+
+pi "# Build native image using container"
+pe "./mvnw clean package -Dnative -Dquarkus.native.container-build=true"
+
+pi "# End"
+# Return to the default PWD
+cd ${_PWD}
+
+pe ""
